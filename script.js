63b4a878bc433635534091ba6c25375571445709
Dhawal Raturi <dhawalraturi@gmail.com>
2021-11-08 14:07:20 +0530

    add starting project

---
+++ script.js
@@ -0,0 +1,40 @@
+let css = document.querySelector('h3'); 
+
+let color1 = document.querySelector('.color1');
+let color2 = document.querySelector('.color2'); 
+let body = document.getElementById('gradient'); 
+let ctxt1 = document.getElementById('crtxt1');
+let ctxt2 = document.getElementById('crtxt2');
+let button = document.getElementById('random'); 
+
+
+function setGradient(){
+	body.style.background = "linear-gradient(to right," 
+	+ color1.value 
+	+", " 
+	+ color2.value 
+	+ ")"; 
+
+	css.innerHTML = "background: " + body.style.background + ';'; 
+	ctxt1.textContent= 'COLOR I:' + color1.value;
+	ctxt2.textContent= 'COLOR II:' + color2.value;
+
+}
+
+
+function generateRandomColor() {
+var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+  return randomColor;
+}
+
+function setRandomGradient(){
+	color1.value = generateRandomColor(); 
+	color2.value = generateRandomColor(); 
+	setGradient(); 
+}
+
+color1.addEventListener('input', setGradient); 
+
+color2.addEventListener('input', setGradient); 
+
+button.addEventListener('click', setRandomGradient); 
